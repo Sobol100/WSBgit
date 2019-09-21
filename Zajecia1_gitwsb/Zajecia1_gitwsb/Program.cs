2f27078496d1724e58328423f1327f0841bb621c
Karol Sobisz <karolsobisz@gmail.com>
2019-09-21 13:18:25 +0200

    using System; using System.Collections.Generic; using System.Linq; using System.Text; using System.Threading.Tasks; using System.IO; using System.Linq; using System.Text.RegularExpressions;
    namespace gitWsb
    {
        class Program
        {
            const string UsersFile = "user.txt";
            const string BooksFile = "books.txt";
            static void Main(string[] args)
            {
    
                string id;
                string password;
                if (!File.Exists(UsersFile))
                    throw new Exception("Cannot find user.txt!");
                if (!File.Exists(BooksFile))
                    throw new Exception("Cannot find book.txt!");
    
                //Console.WriteLine("Login:");
                //id = Console.ReadLine();
                //Console.WriteLine("Password:");
                //password = Console.ReadLine();
    
                StreamReader sr = new StreamReader("user.txt");
                string file = sr.ReadToEnd();
    
                string[] user = file.Split('\n');
                string[] login = uer[0];//.Split('\r');
    
                Console.ReadKey();
            }
        }
        class User
        {
            string Id;
            string Password;
        }
    }

---
+++ Zajecia1_gitwsb/Zajecia1_gitwsb/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace gitWsb
+{
+    class Program
+    {
+        const string UsersFile = "user.txt";
+        const string BooksFile = "books.txt";
+        static void Main(string[] args)
+        {
+
+            string id;
+            string password;
+            if (!File.Exists(UsersFile))
+                throw new Exception("Cannot find user.txt!");
+            if (!File.Exists(BooksFile))
+                throw new Exception("Cannot find book.txt!");
+
+            //Console.WriteLine("Login:");
+            //id = Console.ReadLine();
+            //Console.WriteLine("Password:");
+            //password = Console.ReadLine();
+
+            StreamReader sr = new StreamReader("user.txt");
+            string file = sr.ReadToEnd();
+
+            string[] user = file.Split('\n');
+            string[] login = user[0];//.Split('\r');
+
+            Console.ReadKey();
+        }
+    }
+    class User
+    {
+        string Id;
+        string Password;
+    }
+}
